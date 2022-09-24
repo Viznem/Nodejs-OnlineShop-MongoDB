dc67c24fcf887d34f9ee49a0086f802dad3e23bb
Viznem <thinhtruong3344@gmail.com>
2022-09-24 11:13:15 +0700

    first commit

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Viznem3344!'
+});
+
+module.exports = pool.promise();
